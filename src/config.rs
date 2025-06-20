43b48d0aaef56f5b9ff952ffa2f0edb0ecefca65
mghost7711 <639124483@qq.com>
2025-06-20 16:52:07 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["111.231.65.117"];
+pub const RS_PUB_KEY: &str = "BJeU5aPSPQAO0c7D30LKbxtunkZJQtMLdDS5KWhUzpw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
